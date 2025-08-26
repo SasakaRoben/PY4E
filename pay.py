c3a235d97bc449eb2b57a910344e2e51c757b24b
SasakaRoben <sasakarobenld@gmail.com>
2025-08-26 20:20:55 +0300

    Refactor pay computation into a function

--- pay.py
+++ pay.py
@@ -1,13 +1,16 @@
-try:
-    hours = float(input("Enter Hours: "))
-    rate = float(input("Enter Rate Per Hour: "))
-
+def compute_pay(hours, rate):
     if hours > 40:
         extra_hours = hours - 40
         pay = (40 * rate) + (extra_hours * rate * 1.5)
     else:
         pay = hours * rate
+    return pay
 
+try:
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate Per Hour: "))
+
+    pay = compute_pay(hours, rate)
     print("Gross Pay:", pay)
 except:
     print("Error, please enter numeric input")
