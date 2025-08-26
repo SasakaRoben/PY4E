49aa59ac73281199e779fc3f8f550e99df6590e2
SasakaRoben <sasakarobenld@gmail.com>
2025-08-26 17:35:14 +0300

    Add exception handling for non-numeric input

--- pay.py
+++ pay.py
@@ -1,10 +1,13 @@
-hours = float(input("Enter Hours: "))
-rate = float(input("Enter Rate Per Hour: "))
+try:
+    hours = float(input("Enter Hours: "))
+    rate = float(input("Enter Rate Per Hour: "))
 
-if hours > 40:
-    extra_hours = hours - 40
-    pay = (40 * rate) + (extra_hours * rate * 1.5)
-else:
-    pay = hours * rate
+    if hours > 40:
+        extra_hours = hours - 40
+        pay = (40 * rate) + (extra_hours * rate * 1.5)
+    else:
+        pay = hours * rate
 
-print("Gross Pay:", pay)
+    print("Gross Pay:", pay)
+except:
+    print("Error, please enter numeric input")
