5476739f1c9ee8922a0a7a3320a9b19d6121de61
SasakaRoben <sasakarobenld@gmail.com>
2025-08-27 11:57:19 +0300

    Store the largest and smallest numbers

--- aggregate.py
+++ aggregate.py
@@ -1,20 +1,31 @@
 total = 0
 count = None
+largest = None
+smallest = None
 
 while True:
     inp = input("Enter a number: ")
     if inp == "done":
         break
+
     try:
         num = float(inp)
     except:
         print("Invalid input")
         continue
+
     total += num
+
     if count is None:
         count = 1
     else:
         count += 1
+    
+    if largest is None or num > largest:
+        largest = num
+    
+    if smallest is None or num < smallest:
+        smallest = num
 
 average = total / count
 print("Count:", count)
