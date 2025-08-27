ee1efdac8461b91d8a30bf66611917ff40893115
SasakaRoben <sasakarobenld@gmail.com>
2025-08-27 13:09:54 +0300

    Use in-built count function instead of user-defined one

--- string_count.py
+++ string_count.py
@@ -1,10 +1,3 @@
-def count(string, letter):
-    count = 0
-    for char in string:
-        if char == letter:
-            count += 1
-    return count
-
 word = 'banana'
 letter = 'a'
-print("The letter ", letter, " appears ", count(word, letter), " times in the word ", word)
+print("The letter ", letter, " appears ", word.count(letter), " times in the word ", word)
