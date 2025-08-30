08ed409bb299e89cdb60d59f9088a20958eb27dc
SasakaRoben <sasakarobenld@gmail.com>
2025-08-30 10:38:47 +0300

    Skip lines with zero words

--- days.py
+++ days.py
@@ -1,4 +1,5 @@
 fhand = open(mbox-short.txt)
 for line in fhand:
     words = line.split()
-    if words[0] != From : continue
+    if len(words) == 0 : continue
+    if words[0] != 'From' : continue
