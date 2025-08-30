5041aca4c79efe6ebf42d50d92f4434431143c30
SasakaRoben <sasakarobenld@gmail.com>
2025-08-30 08:43:23 +0300

    Remove middle elements in a list
    The essence of the two functions was to differentiate between modifying a list and creating a new list. New lists are supposed to be explicitly returned while modified lists are impllicitly returned since the caller sees the change

--- middle_chop.py
+++ middle_chop.py
@@ -7,3 +7,17 @@ def chop(names):
         print('Chop Failed; list should have 2 or more elements')
     return None
 
+def middle(names):
+    if len(names) > 2:
+        return names[1:-1]
+    else:
+        print('Middle Failed; list should have 3 or more elements')
+        return None
+
+print('Original List:', names)
+chop(names)
+print('After chop():', names)
+names = ['Roben', 'Sasaka', 'Melody', 'Murengu', 'Moses', 'Muiga', 'Mutahi']
+print('Original List:', names)
+print('After middle():', middle(names))
+
