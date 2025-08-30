fceaa4d0b1bfa331fe4425af6c152313c4d3eb63
SasakaRoben <sasakarobenld@gmail.com>
2025-08-30 10:44:41 +0300

    Skip lines with less than 3 words

--- days.py
+++ days.py
@@ -1,5 +1,7 @@
-fhand = open(mbox-short.txt)
+fhand = open('mbox-short.txt')
 for line in fhand:
     words = line.split()
     if len(words) == 0 : continue
     if words[0] != 'From' : continue
+    if len(words) < 3 : continue
+    print(words[2])
