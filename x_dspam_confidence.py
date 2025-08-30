fe89974db0a3f19f6b3991f6a9885844795e8680
SasakaRoben <sasakarobenld@gmail.com>
2025-08-30 07:17:38 +0300

    Extract floating point number on the lines

--- x_dspam_confidence.py
+++ x_dspam_confidence.py
@@ -1,6 +1,11 @@
 fname = './mbox-short.txt'
 count = 0
+total = 0.0
 fhand = open(fname)
 for line in fhand:
     line = line.rstrip()
-    if line.startswith('X-DSPAM-Confidence:'): 
+    if line.startswith('X-DSPAM-Confidence:'):
+        colon_pos = line.find(':')
+        number = line[colon_pos + 1:]
+        number = float(number)
+        
