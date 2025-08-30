9abd8556d0fb7281f8960a81dd415b373e59540a
SasakaRoben <sasakarobenld@gmail.com>
2025-08-30 10:48:56 +0300

    Refactor conditonals into one check

--- days.py
+++ days.py
@@ -1,7 +1,6 @@
 fhand = open('mbox-short.txt')
 for line in fhand:
     words = line.split()
-    if len(words) == 0 : continue
-    if words[0] != 'From' : continue
-    if len(words) < 3 : continue
+    if (len(words) == 0) or (words[0] != 'From') or (len(words) < 3):
+        continue
     print(words[2])
