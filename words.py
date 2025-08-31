eb75e8fef48ece20c39f7bf0ca02368551e4a224
SasakaRoben <sasakarobenld@gmail.com>
2025-08-31 08:52:02 +0300

    Create two lists of unique words

---
+++ words.py
@@ -0,0 +1,26 @@
+"""
+Write a program that reads the words in `words.txt` and stores them as keys in 
+a dictionary. It doesn’t matter what the values are. 
+Then you can use the `in` operator as a fast way to check whether a string is in the dictionary.
+
+1. Create 2 lists of unique words from the file `words.txt` and `romeo.txt`
+2. If the words in `romeo.txt` are more than the ones in `words.txt`:
+    Trim the list of words in `romeo.txt` to be the same length as the list of words in `words.txt`
+3. Create a dictionary with the words in `words.txt` as keys and the words in `romeo.txt` as values
+"""
+unique_keys = list()
+unique_values = list()
+fhand = open('words.txt')
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in unique_keys:
+            unique_keys.append(word)
+
+fhand = open('romeo.txt')
+for line in fhand:
+    words = line.split()
+    for word in words:
+        if word not in unique_values:
+            unique_values.append(word)
+
